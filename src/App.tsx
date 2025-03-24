a8bbd839c07d0b107b8e6174c76160d1f4622e4b
granteckels <geckels1@gmail.com>
2025-03-23 21:01:59 -0500

    Finish Navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Nav from './components/Nav';
 
 function App() {
+  const location = useLocation().pathname.slice(1);
+
   return (
     <>
-      <Nav />
+      <Nav location={location} />
       <main>
         <Outlet />
       </main>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,18 @@
-const Nav = () => {
+import { Link } from 'react-router-dom'
+
+interface NavProps {
+  location: string;
+}
+
+const Nav = ({ location }: NavProps) => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <nav>
+      <ul className="nav">
+        <li className="nav-item"><Link className={`nav-link ${location === "" ? "active" : ""}`} to="/">Search for Candidate</Link></li>
+        <li className="nav-item"><Link className={`nav-link ${location === "SavedCandidates" ? "active" : ""}`} to="/SavedCandidates">Potential Candidates</Link></li>
+      </ul>
+    </nav>
   )
 };
 
